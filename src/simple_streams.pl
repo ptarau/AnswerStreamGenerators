5638732d1516e753a1e79718e47ec7ac39822618
Paul Tarau <ptarau@gmail.com>
2019-04-08 20:16:40 -0500

    standardized tests in simple_streams.pl, added output as comment

--- src/simple_streams.pl
+++ src/simple_streams.pl
@@ -20,6 +20,10 @@ ask_(E,_):-is_done(E),!,fail.
 ask_(E,R):-call(E,X),!,R=X.
 ask_(E,_):-stop_(E),fail.
 
+show(K,Stream):-once(findnsols(K,X, X in Stream,Xs)),writeln(Xs).
+
+show(Stream):-show(12,Stream).
+
 % constant infinite stream returning C
 const_(C,=(C)).
 
@@ -31,13 +35,13 @@ stream_next(F,State,X):-
   call(F,X,Y),
   nb_linkarg(1,State,Y).
 
-nat_stream(S,X):-stream_next(succ,S,X).
+nat_next(S,X):-stream_next(succ,S,X).
 
 % natural numbers
-nat_(nat_stream(state(0))).
+nat_(nat_next(state(0))).
 
 % stricly positive integers
-pos_(nat_stream(state(1))).
+pos_(nat_next(state(1))).
 
 % predecessor defined on all integers
 pred(X,PX):-PX is X-1.
@@ -46,9 +50,9 @@ pred(X,PX):-PX is X-1.
 neg_(stream_next(pred,state(-1))).
 
 % finite stream from list
-list_(Xs,list_stream(state(Xs))).
+list_(Xs,list_next(state(Xs))).
 
-list_stream(State,X):-  
+list_next(State,X):-  
   arg(1,State,[X|Xs]),
   nb_linkarg(1,State,Xs).
 
@@ -56,9 +60,9 @@ eng_(X,G,engine_next(E)):-engine_create(X,G,E).
 
 
 % finite integer range
-range_(From,To,range_stream(state(From,To))).
+range_(From,To,range_next(state(From,To))).
 
-range_stream(State,X):-
+range_next(State,X):-
   State=state(X,To),
   X<To,
   succ(X,SX),
@@ -82,17 +86,17 @@ slice(From,To)-->{K is To-From,K>=0},drop(From),take(K).
 
 
 % lazy functional operators  
-map_(F,E,map_stream(F,E)).
+map_(F,E,map_next(F,E)).
 
-map_stream(F,E,Y):-ask_(E,X),call(F,X,Y).
+map_next(F,E,Y):-ask_(E,X),call(F,X,Y).
 
-map_(F,E1,E2,map_stream(F,E1,E2)).
+map_(F,E1,E2,map_next(F,E1,E2)).
 
-map_stream(F,E1,E2,Z):-ask_(E1,X),ask_(E2,Y),call(F,X,Y,Z).
+map_next(F,E1,E2,Z):-ask_(E1,X),ask_(E2,Y),call(F,X,Y,Z).
 
-reduce_(F,InitVal,E,reduce_stream(state(InitVal),F,E)).
+reduce_(F,InitVal,E,reduce_next(state(InitVal),F,E)).
 
-reduce_stream(S,F,E,R):-
+reduce_next(S,F,E,R):-
   \+ is_done(E),
   do((
     Y in E,
@@ -102,31 +106,35 @@ reduce_stream(S,F,E,R):-
   )),
   arg(1,S,R).
 
+  
+% bactrack over G for its side-effects only  
+do(G):-G,fail;true.
+
 
 % sequence sum and product operrations  
 
 % interleaved sum of two finite or infinite streams
-sum_(E1,E2,sum_stream(state(E1,E2))).
+sum_(E1,E2,sum_next(state(E1,E2))).
 
-sum_stream(State,X):-
+sum_next(State,X):-
   State=state(E1,E2),
   ask_(E1,X),
   !,
   nb_linkarg(1,State,E2),
   nb_linkarg(2,State,E1).
-sum_stream(state(_,E2),X):-
+sum_next(state(_,E2),X):-
   ask_(E2,X).
 
 % cartesian product of two finite or infinite streams
-prod_(E1,E2,prod_stream(state(0,E1,E2,A1,A2))):-
+prod_(E1,E2,prod_next(state(0,E1,E2,A1,A2))):-
   new_array(A1),
   new_array(A2).
 
-prod_stream(S,X-Y):-
+prod_next(S,X-Y):-
   S=state(_,E1,E2,A1,A2),
   repeat,
     ( is_done(E1),is_done(E2) -> !,fail
-    ; nat_pair_stream(S,I-J),
+    ; nat_pair_next(S,I-J),
       fill_to(I,E1,A1,X),
       fill_to(J,E2,A2,Y)
     ),
@@ -141,14 +149,11 @@ fill_to(N,E,A,R):-
   )),
   array_get(A,N,R),
   nonvar(R).
-
-% bactrack over G for its side-effects only  
-do(G):-G,fail;true.
-
+  
 % stream of naturla number pairs
-nat_pair_(nat_pair_stream(state(0))).
+nat_pair_(nat_pair_next(state(0))).
 
-nat_pair_stream(S,A-B):-nat_stream(S,X),cantor_unpair(X,A,B).
+nat_pair_next(S,A-B):-nat_next(S,X),cantor_unpair(X,A,B).
 
 
 % cantor pairing function
@@ -191,49 +196,228 @@ X in_ E:-eeval(E,EE),X in EE.
 % tests
 
   
-f1(X):-nat_(N),list_([10,20,30],M),map_(plus,N,M,R),X in R.
+t1:-nat_(N),list_([10,20,30],M),map_(plus,N,M,R),show(R).
  
-f2(X):-nat_(N),nat_(M),map_(plus,N,M,R),X in R.  
+t2:-nat_(N),nat_(M),map_(plus,N,M,R),show(R).  
 
-f3(X):-range_(1,5,E),reduce_(plus,0,E,R),X in R.
+t3:-range_(1,5,E),reduce_(plus,0,E,R),show(R).
 
-t1(X):-pos_(N),neg_(M),sum_(M,N,S),X in S. 
+t4:-pos_(N),neg_(M),sum_(M,N,S),show(S). 
 
-t2(X):-nat_(N),list_([a,b,c],M),sum_(N,M,S),X in S.
+t5:-nat_(N),list_([a,b,c],M),sum_(N,M,S),show(S).
 
-t3(X):-range_(1,3,N),list_([a,b,c,d,e],M),sum_(M,N,S),X in S.
+t6:-range_(1,3,N),list_([a,b,c,d,e],M),sum_(M,N,S),show(S).
   
-t4(X):-nat_(N),slice(4,8,N,S),X in S.
+t7:-nat_(N),slice(4,8,N,S),show(S).
 
-t5:-neg_(A),pos_(B),prod_(A,B,P),
-   take(30,P,T),forall(X in T,writeln(X)).
+t8:-neg_(A),pos_(B),prod_(A,B,P),
+   take(30,P,T),show(30,T).
 
-t6:-nat_(A),list_([a,b,c],B),prod_(A,B,P),
-    take(30,P,T),forall(X in T,writeln(X)).
+t9:-nat_(A),list_([a,b,c],B),prod_(A,B,P),
+    take(20,P,T),forall(X in T,writeln(X)).
 
-t7:-range_(0,5,A),list_([a,b,c],B),prod_(A,B,P),
-    take(30,P,T),forall(X in T,writeln(X)).
+t10:-range_(0,5,A),list_([a,b,c],B),prod_(A,B,P),
+    take(20,P,T),show(30,T).
     
-t8:-nat_(A),list_([a,b,c],B),
-  prod_(B,A,P),take(30,P,T),
-  forall(X in T,writeln(X)).
+t11:-nat_(A),list_([a,b,c],B),
+  prod_(B,A,P),take(20,P,T),
+  show(30,T).
   
-t9(X):-const_(10,C),nat_(N),map_(plus,C,N,R),X in R.
+t12:-const_(10,C),nat_(N),map_(plus,C,N,R),show(R).
 
-t10(X):-const_(10,C),nat_(N),prod_(C,N,P),X in P.
+t13:-const_(10,C),nat_(N),prod_(C,N,P),show(P).
 
 
 odds(Xs) :-
   lazy_findall(X, (between(0, infinite, X0),X is 2*X0+1), Xs).
 
-t11(X):-odds(Xs),list_(Xs,L),nat_(N),prod_(L,N,P),X in P.
+t14:-odds(Xs),list_(Xs,L),nat_(N),prod_(L,N,P),show(P).
   
-e1(X):-eng_(X,member(X,[1,2,3]),E),list_([a,b],L),sum_(E,L,S),X in S.
+t15:-eng_(X,member(X,[1,2,3]),E),list_([a,b],L),sum_(E,L,S),show(S).
 
-e2(X):-eng_(X,member(X,[1,2,3]),E),list_([a,b],L),prod_(E,L,S),X in S.
+t16:-eng_(X,member(X,[1,2,3]),E),list_([a,b],L),prod_(E,L,S),show(S).
 
-e3:-eng_(X,member(X,[1,2,3]),S),(X in S,writeln(X),fail;is_done(S),writeln(S)).
+t17:-eng_(X,member(X,[1,2,3]),S),(X in S,writeln(X),fail;is_done(S),writeln(S)).
 
-e4:-(X^member(X,[1,2,3])*[a,b,c,d])=E,do((X in_ E,writeln(X))).
+t18:-(X^member(X,[1,2,3])*[a,b])=E,do((X in_ E,writeln(X))).
+
+tests:-do((between(1,18,I),atom_concat(t,I,T),listing(T),call(T),nl)).
+
+/*
+?- tests.
+t1 :-
+    nat_(N),
+    list_([10, 20, 30], M),
+    map_(plus, N, M, R),
+    show(R).
+
+[10,21,32]
+
+t2 :-
+    nat_(N),
+    nat_(M),
+    map_(plus, N, M, R),
+    show(R).
+
+[0,2,4,6,8,10,12,14,16,18,20,22]
+
+t3 :-
+    range_(1, 5, E),
+    reduce_(plus, 0, E, R),
+    show(R).
+
+[10]
+
+t4 :-
+    pos_(N),
+    neg_(M),
+    sum_(M, N, S),
+    show(S).
+
+[-1,1,-2,2,-3,3,-4,4,-5,5,-6,6]
+
+t5 :-
+    nat_(N),
+    list_([a, b, c], M),
+    sum_(N, M, S),
+    show(S).
+
+[0,a,1,b,2,c,3,4,5,6,7,8]
+
+t6 :-
+    range_(1, 3, N),
+    list_([a, b, c, d, e], M),
+    sum_(M, N, S),
+    show(S).
+
+[a,1,b,2,c,d,e]
+
+t7 :-
+    nat_(N),
+    slice(4, 8, N, S),
+    show(S).
+
+[4,5,6,7]
+
+t8 :-
+    neg_(A),
+    pos_(B),
+    prod_(A, B, P),
+    take(30, P, T),
+    show(30, T).
+
+[-1-1,-2-1,-1-2,-3-1,-2-2,-1-3,-4-1,-3-2,-2-3,-1-4,-5-1,-4-2,-3-3,-2-4,-1-5,-6-1,-5-2,-4-3,-3-4,-2-5,-1-6,-7-1,-6-2,-5-3,-4-4,-3-5,-2-6,-1-7,-8-1,-7-2]
+
+t9 :-
+    nat_(A),
+    list_([a, b, c], B),
+    prod_(A, B, P),
+    take(20, P, T),
+    \+ ( X in T,
+         \+ writeln(X)
+       ).
+
+0-a
+1-a
+0-b
+2-a
+1-b
+0-c
+3-a
+2-b
+1-c
+4-a
+3-b
+2-c
+5-a
+4-b
+3-c
+6-a
+5-b
+4-c
+7-a
+6-b
+
+t10 :-
+    range_(0, 5, A),
+    list_([a, b, c], B),
+    prod_(A, B, P),
+    take(20, P, T),
+    show(30, T).
+
+[0-a,1-a,0-b,2-a,1-b,0-c,3-a,2-b,1-c,4-a,3-b,2-c]
+
+t11 :-
+    nat_(A),
+    list_([a, b, c], B),
+    prod_(B, A, P),
+    take(20, P, T),
+    show(30, T).
+
+[a-0,b-0,a-1,c-0,b-1,a-2,c-1,b-2,a-3,c-2,b-3,a-4,c-3,b-4,a-5,c-4,b-5,a-6,c-5,b-6]
+
+t12 :-
+    const_(10, C),
+    nat_(N),
+    map_(plus, C, N, R),
+    show(R).
+
+[10,11,12,13,14,15,16,17,18,19,20,21]
+
+t13 :-
+    const_(10, C),
+    nat_(N),
+    prod_(C, N, P),
+    show(P).
+
+[10-0,10-0,10-1,10-0,10-1,10-2,10-0,10-1,10-2,10-3,10-0,10-1]
+
+t14 :-
+    odds(Xs),
+    list_(Xs, L),
+    nat_(N),
+    prod_(L, N, P),
+    show(P).
+
+[1-0,3-0,1-1,5-0,3-1,1-2,7-0,5-1,3-2,1-3,9-0,7-1]
+
+t15 :-
+    eng_(X, member(X, [1, 2, 3]), E),
+    list_([a, b], L),
+    sum_(E, L, S),
+    show(S).
+
+[1,a,2,b,3]
+
+t16 :-
+    eng_(X, member(X, [1, 2, 3]), E),
+    list_([a, b], L),
+    prod_(E, L, S),
+    show(S).
+
+[1-a,2-a,1-b,3-a,2-b]
+
+t17 :-
+    eng_(X, member(X, [1, 2, 3]), S),
+    (   X in S,
+        writeln(X),
+        fail
+    ;   is_done(S),
+        writeln(S)
+    ).
+
+1
+2
+3
+engine_next(done)
+
+1-a
+2-a
+1-b
+3-a
+2-b
+
+true.
 
 
+*/
