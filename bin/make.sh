4e83c344aab1d628d58aa8ad8c33b56fdf12ab76
Paul Tarau <ptarau@gmail.com>
2019-04-10 17:59:08 -0500

    changed run.sh to cover both ansgens and gens files

---
+++ bin/make.sh
@@ -0,0 +1,22 @@
+export PFILE=$1
+
+# make code extractor
+gcc -O -o toc ../src/toc.c
+
+# make latex
+pushd .
+cd ../src
+rm -f $PFILE.pdf
+pdflatex $PFILE.tex
+pdflatex $PFILE.tex
+bibtex $PFILE
+pdflatex $PFILE.tex
+ls -l  $PFILE.pdf
+
+# extract Prolog
+rm -f  $PFILE.pro
+../bin/toc < $PFILE.tex > $PFILE.pro
+ls -l $PFILE.pro
+swipl -O -s $PFILE.pro
+popd
+
--- bin/run.sh
+++ bin/run.sh
@@ -1,21 +1,2 @@
-export PFILE=ansgens
-# make code extractor
-gcc -O -o toc ../src/toc.c
-
-# make latex
-pushd .
-cd ../src
-rm -f $PFILE.pdf
-pdflatex $PFILE.tex
-pdflatex $PFILE.tex
-bibtex $PFILE
-pdflatex $PFILE.tex
-ls -l  $PFILE.pdf
-
-# extract Prolog
-rm -f  $PFILE.pro
-../bin/toc < $PFILE.tex > $PFILE.pro
-ls -l $PFILE.pro
-swipl -O -s $PFILE.pro
-popd
-
+make.sh ansgens
+make.sh gens
