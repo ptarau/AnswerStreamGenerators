a18dd056ec7ef327051759597775edcad3f8a519
Paul Tarau <ptarau@gmail.com>
2019-04-15 23:37:45 -0500

    separation of tests.pl, test-driven fixes

--- src/generators.pl
+++ src/generators.pl
@@ -1,4 +1,4 @@
-c:-['generators.pl'].
+cc:-['generators.pl'].
 
 :-ensure_loaded(arrays).
 
@@ -73,6 +73,11 @@ gen_nextval(F,State,Yield):-
   call(F,X1,X2, Yield),
   nb_linkarg(1,State,X2).
 
+gen_safe_nextval(F,State,Yield):-
+  arg(1,State,X1),
+  call(F,X1,X2, Yield),
+  nb_setarg(1,State,X2).
+  
 % stream from list or lazy_list   
 list_(Xs,gen_nextval(list_step,state(Xs))).
 list_step([X|Xs],Xs,X).
@@ -239,9 +244,11 @@ prod_(E1,E2,prod_next(state(0,E1,E2,A1,A2))):-
 
 prod_next(S,X-Y):-
   S=state(_,E1,E2,A1,A2),
+  %conv_(C),
   repeat,
     ( is_done(E1),is_done(E2) -> !,fail
     ; nat_pair_next(S,I-J),
+      %ask_(C,I-J),ppp(I-J),
       fill_to(I,E1,A1,X),
       fill_to(J,E2,A2,Y)
     ),
@@ -292,12 +299,15 @@ conv_pairs(0,L,[L-0]).
 conv_pairs(K,L,[L-K|Ps]):-succ(PK,K),succ(L,SL),conv_pairs(PK,SL,Ps).
   
 
-conv_(gen_nextval(conv_step,state(0-[0-0]))).
+conv_(gen_safe_nextval(conv_step,state(0-[0-0]))).
 
 conv_step(N-[X|Xs],N-Xs,X).
 conv_step(N-[],SN-Xs,X):-succ(N,SN),conv_pairs(SN,[X|Xs]).
   
   
+%nobug:-conv_(C),(ask_(C,_),ask_(C,_),ask_(C,_),fail;ask_(C,B)),writeln(B).
+
+
 % ISO-FUNCTOR TO lazy_lists
 
 % data transformers
@@ -395,129 +405,3 @@ add(s(X),Y,s(Z)):-add(X,Y,Z).
 goal(add(s(s(0)),s(s(0)),_R)).
 */
 
-% tests
-
-  
-t1:-nat_(N),list_([10,20,30],M),map_(plus,N,M,R),show(R).
- 
-t2:-nat_(N),nat_(M),map_(plus,N,M,R),show(R).  
-
-t3:-range_(1,5,E),reduce_(plus,0,E,R),show(R).
-
-t4:-pos_(N),neg_(M),sum_(M,N,S),show(S). 
-
-t5:-nat_(N),list_([a,b,c],M),sum_(N,M,S),show(S).
-
-t6:-range_(1,3,N),list_([a,b,c,d,e],M),sum_(M,N,S),show(S).
-  
-t7:-nat_(N),slice(4,8,N,S),show(S).
-
-t8:-neg_(A),pos_(B),prod_(A,B,P),
-   take(30,P,T),show(30,T).
-
-t9:-nat_(A),list_([a,b,c],B),prod_(A,B,P),
-    take(20,P,T),forall(X in T,writeln(X)).
-
-t10:-range_(0,5,A),list_([a,b,c],B),prod_(A,B,P),
-    take(20,P,T),show(30,T).
-    
-t11:-nat_(A),list_([a,b,c],B),
-  prod_(B,A,P),take(20,P,T),
-  show(30,T).
-  
-t12:-const_(10,C),nat_(N),map_(plus,C,N,R),show(R).
-
-t13:-const_(10,C),nat_(N),prod_(C,N,P),show(P).
-
-
-t14:-eng_(_X,fail,E),list_([a,b],L),sum_(E,L,S),show(S).
-  
-t15:-eng_(X,member(X,[1,2,3]),E),list_([a,b],L),sum_(E,L,S),show(S).
-
-t16:-eng_(X,member(X,[1,2,3]),E),list_([a,b],L),prod_(E,L,S),show(S).
-
-t17:-eng_(X,member(X,[1,2,3]),S),(X in S,writeln(X),fail;is_done(S),writeln(S)).
-
-t18:-(X^member(X,[1,2,3])*[a,b])=E,do((X in_ E,writeln(X))).
-
-t19:-range_(1,5,R),cycle_(R,C),show(20,C).
-
-t20:-range_(1,4,R),cycle_(R,C),list_([a,b,c,d,e,f],L),zipper_of(C,L,Z),show(Z).
-
-t21:-eng_(X,member(X,[a,b,c]),G),range_(1,6,R),prod_(G,R,P),show(P).
-
-t22:-ceng_(X,member(X,[a,b,c]),G),ceng_clone(G,CG),prod_(G,CG,P),show(P).
-
-t23:-ceng_(X,member(X,[a,b,c]),G),cycle_(G,C),show(C).
-
-
-t24:-range_(0,10,A),range_(100,110,B),arith_sum(A,B,S),show(S).
-
-t25:-fact(5,S),show(S).
-
-t26:-nat_(N),chains_([succ,succ],N,N2),show(N2).
-
-t27:-fibo_(E),show(E).
-
-t28:-
-  clause_(chains_(_,_,_),C),
-  do((X in C,portray_clause(X))).
-
-t29:-pos_(E),chains_([succ,pred],E,R),show(R).
-
-t30:-pos_(E),mplex_([succ,pred],E,R),show(R).
-
-t31:-lazy_nats(Ls),list_(Ls,E),show(E).
-
-
-t32:-range_(1,10,N),iso_fun(maplist(succ),gen2lazy,lazy2gen,N,M),show(M).
-
-% while maplist loops, this iso functor based map does not
-t33:-lazy_nats(Ns),
-  maplist_(succ,Ns,Ms),
-  once(findnsols(10,I,member(I,Ms),Rs)),
-  writeln(Rs).
-
-odds(Xs) :-lazy_findall(X, (between(0, infinite, X0),X is 2*X0+1), Xs).
-
-% lazy_findall leaves undead engine
-t34:-odds(Xs),list_(Xs,L),nat_(N),prod_(L,N,P),show(P).
-
-tests:-
-  tell('tests.txt'),
-  do((between(1,34,I),atom_concat(t,I,T),listing(T),call(T),nl)),
-  do((current_engine(E),writeln(E))),
-  bm,
-  told.
-
-time(G,T):-get_time(T1),once(G),get_time(T2),T is T2-T1. 
-  
-bm1(K):-
-  nl,listing(bm1),
-  N is 2^K,writeln(with_lazy_lists:N),
-  lazy_findall(I,between(0,N,I),Is),
-  maplist(succ,Is,Js),last(Js,X),writeln([X]).
-
-bm2(K):-
-  nl,listing(bm2),
-  N is 2^K,N1 is N+1,
-  writeln(with_engine_based_generators:N),
-  eng_(I,between(0,N,I),R),
-  map_(succ,R,SR),
-  slice(N,N1,SR,S),
-  show(S).
-  
-bm3(K):-
-  nl,listing(bm3),
-  N is 2^K,N1 is N+1,
-  writeln(with_simple_generators:N),
-  range_(0,N1,R),
-  map_(succ,R,SR),
-  slice(N,N1,SR,S),
-  show(S).
-  
-bm(K):-maplist(time,[bm1(K),bm2(K),bm3(K)],Ts),nl,writeln(times=Ts).
-
-bm:-bm(21).  
-  
-ppp(X):-writeln(X).
