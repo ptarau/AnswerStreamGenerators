9fd2150dbd1d373453c0683e3bfb123b3135b6c3
Paul Tarau <ptarau@gmail.com>
2019-05-15 09:26:14 -0500

    updated tests with lazy_lists_from_to

--- lazy_streams-0.5.0/prolog/test_lazy_streams.pl
+++ lazy_streams-0.5.0/prolog/test_lazy_streams.pl
@@ -276,6 +276,7 @@ bm_last(N):-
  list(Ns,Gen),
  eng(X,member(X,Ns),Eng),
  lazy_findall(X,member(X,Ns),Ls),
+ succ(N,SN),test:lazy_nats_from_to(0,SN,LLs),
  range(0,N,Range),
  
  % access last element
@@ -283,21 +284,25 @@ bm_last(N):-
  time(do(nth(N,Gen,_))),
  time(do(nth(N,Range,_))),
  time(do(nth(N,Eng,_))),
- time(do(nth0(N,Ls,_))).
+ time(do(nth0(N,Ls,_))),
+ time(do(nth0(N,LLs,_))).
  
 bm_all(N):-
  numlist(0,N,Ns), 
  list(Ns,Gen),
  eng(X,member(X,Ns),Eng),
  lazy_findall(X,member(X,Ns),Ls),
+ succ(N,SN),test:lazy_nats_from_to(0,SN,LLs),
  range(0,N,Range),
  % iterating over all, via backtracking
  time(do((member(_,Ns)))),
  time(do((_ in Gen))),
  time(do((_ in Range))),
  time(do((_ in Eng))),
- time(do((member(_,Ls)))).
+ time(do((member(_,Ls)))),
+ time(do((member(_,LLs)))).
 
+ 
 %! bmx
 % 
 %  benchmarks comparing plain Prolog with lazy streams and lazy lists
@@ -327,74 +332,86 @@ bmp(N,Op):-
 
 % Run on a 128GB 18-core iMacPro, with 8 GB given to Prolog to avoid stack overflows with lazy lists.
 
-?- lazy_streams:bmx.
-% 75,497,485 inferences, 3.004 CPU in 3.008 seconds (100% CPU, 25133297 Lips)
-% 41,943,046 inferences, 13.957 CPU in 16.543 seconds (84% CPU, 3005098 Lips)
-% 41,943,046 inferences, 13.098 CPU in 15.602 seconds (84% CPU, 3202354 Lips)
-% 120,236,795 inferences, 18.022 CPU in 18.823 seconds (96% CPU, 6671800 Lips)
-% 128,625,342 inferences, 25.965 CPU in 28.634 seconds (91% CPU, 4953776 Lips)
-% Started at Fri May 10 13:32:54 2019
-% 82.709 seconds cpu time for 409,024,763 inferences
-% 7,436 atoms, 4,428 functors, 3,471 predicates, 68 modules, 130,130 VM-codes
+?- bmx.
+% 75,497,483 inferences, 2.999 CPU in 3.004 seconds (100% CPU, 25174563 Lips)
+% 41,943,046 inferences, 8.137 CPU in 9.606 seconds (85% CPU, 5154863 Lips)
+% 41,943,046 inferences, 7.579 CPU in 9.064 seconds (84% CPU, 5533910 Lips)
+% 95,070,967 inferences, 11.982 CPU in 12.453 seconds (96% CPU, 7934266 Lips)
+% 128,625,340 inferences, 21.734 CPU in 24.157 seconds (90% CPU, 5918102 Lips)
+% Started at Wed May 15 09:19:47 2019
+% 58.379 seconds cpu time for 383,867,626 inferences
+% 7,453 atoms, 4,443 functors, 3,449 predicates, 69 modules, 130,610 VM-codes
 % 
 %                     Limit   Allocated      In use
 % Local  stack:           -       20 Kb    1,824  b
-% Global stack:           -    2,048 Mb    2,013 Mb
-% Trail  stack:           -    1,024 Mb    3,792  b
-%        Total:    8,192 Mb    3,072 Mb    2,013 Mb
+% Global stack:           -    4,096 Mb    1,691 Mb
+% Trail  stack:           -    2,048 Mb    3,720  b
+%        Total:    8,192 Mb    6,144 Mb    1,691 Mb
 % 
-% 20 garbage collections gained 5,842,469,976 bytes in 14.964 seconds.
-% 2 clause garbage collections gained 109 clauses in 0.000 seconds.
-% Stack shifts: 2 local, 17 global, 17 trail in 0.014 seconds
-% 2 threads, 0 finished threads used 0.000 seconds
-% 3 engines, 0 finished engines
-
-% 2,796,205 inferences, 0.114 CPU in 0.114 seconds (100% CPU, 24576621 Lips)
-% 67,108,873 inferences, 2.791 CPU in 2.793 seconds (100% CPU, 24045947 Lips)
-% 83,886,096 inferences, 3.334 CPU in 3.340 seconds (100% CPU, 25163627 Lips)
-% 41,943,046 inferences, 13.420 CPU in 15.971 seconds (84% CPU, 3125467 Lips)
-% 128,625,340 inferences, 26.703 CPU in 30.097 seconds (89% CPU, 4816896 Lips)
-% Started at Fri May 10 13:32:54 2019
-% 136.603 seconds cpu time for 741,774,100 inferences
-% 7,439 atoms, 4,428 functors, 3,461 predicates, 68 modules, 130,148 VM-codes
-% 
-%                     Limit   Allocated      In use
-% Local  stack:           -       20 Kb    1,824  b
-% Global stack:           -    4,096 Mb    2,181 Mb
-% Trail  stack:           -    2,048 Mb    4,784  b
-%        Total:    8,192 Mb    6,144 Mb    2,181 Mb
-% 
-% 21 garbage collections gained 9,608,162,632 bytes in 21.059 seconds.
-% 2 clause garbage collections gained 109 clauses in 0.000 seconds.
-% Stack shifts: 2 local, 18 global, 18 trail in 0.014 seconds
+% 17 garbage collections gained 5,499,092,552 bytes in 13.547 seconds.
+% 3 clause garbage collections gained 125 clauses in 0.000 seconds.
+% Stack shifts: 2 local, 18 global, 18 trail in 0.095 seconds
 % 2 threads, 0 finished threads used 0.000 seconds
 % 3 engines, 2 finished engines
 
-% 8,388,611 inferences, 0.483 CPU in 0.484 seconds (100% CPU, 17361722 Lips)
-% 75,497,495 inferences, 3.093 CPU in 3.100 seconds (100% CPU, 24410884 Lips)
-% 92,274,703 inferences, 3.689 CPU in 3.696 seconds (100% CPU, 25012260 Lips)
-% 50,331,669 inferences, 13.322 CPU in 15.876 seconds (84% CPU, 3778062 Lips)
-% 134,217,757 inferences, 25.539 CPU in 28.115 seconds (91% CPU, 5255455 Lips)
-% Started at Fri May 10 13:32:54 2019
-% 189.281 seconds cpu time for 1,110,874,042 inferences
-% 7,441 atoms, 4,428 functors, 3,460 predicates, 68 modules, 130,148 VM-codes
+% 2,796,205 inferences, 0.113 CPU in 0.113 seconds (100% CPU, 24789051 Lips)
+% 67,108,873 inferences, 2.908 CPU in 2.912 seconds (100% CPU, 23077131 Lips)
+% 83,886,095 inferences, 3.360 CPU in 3.366 seconds (100% CPU, 24963581 Lips)
+% 41,943,046 inferences, 7.779 CPU in 9.258 seconds (84% CPU, 5391779 Lips)
+% 128,625,340 inferences, 14.303 CPU in 16.110 seconds (89% CPU, 8992591 Lips)
+% 103,459,513 inferences, 12.810 CPU in 13.070 seconds (98% CPU, 8076431 Lips)
+% Started at Wed May 15 09:19:47 2019
+% 104.547 seconds cpu time for 820,076,521 inferences
+% 7,456 atoms, 4,443 functors, 3,443 predicates, 69 modules, 130,610 VM-codes
 % 
 %                     Limit   Allocated      In use
-% Local  stack:           -       20 Kb    1,752  b
-% Global stack:           -    4,096 Mb    2,276 Mb
-% Trail  stack:           -    2,048 Mb    4,784  b
-%        Total:    8,192 Mb    6,144 Mb    2,276 Mb
+% Local  stack:           -       24 Kb    1,824  b
+% Global stack:           -    6,351 Mb    2,251 Mb
+% Trail  stack:           -    1,841 Mb    3,856  b
+%        Total:    8,192 Mb    8,192 Mb    2,251 Mb
 % 
-% 22 garbage collections gained 13,440,569,688 bytes in 26.112 seconds.
-% 2 clause garbage collections gained 109 clauses in 0.000 seconds.
-% Stack shifts: 2 local, 18 global, 18 trail in 0.014 seconds
+% 18 garbage collections gained 11,706,621,416 bytes in 20.518 seconds.
+% 3 clause garbage collections gained 125 clauses in 0.000 seconds.
+% Stack shifts: 3 local, 19 global, 19 trail in 0.197 seconds
 % 2 threads, 0 finished threads used 0.000 seconds
 % 3 engines, 4 finished engines
-true.
 
-?- lazy_streams:bmp.
-% 1,309,715 inferences, 0.423 CPU in 0.502 seconds (84% CPU, 3099866 Lips)
-% 241,963,257 inferences, 8.841 CPU in 8.851 seconds (100% CPU, 27367598 Lips)
+% 8,388,611 inferences, 0.495 CPU in 0.496 seconds (100% CPU, 16949771 Lips)
+% 75,497,495 inferences, 3.248 CPU in 3.258 seconds (100% CPU, 23245090 Lips)
+% 92,274,703 inferences, 3.738 CPU in 3.739 seconds (100% CPU, 24683495 Lips)
+% 50,331,668 inferences, 8.057 CPU in 9.571 seconds (84% CPU, 6246626 Lips)
+% 134,217,757 inferences, 14.664 CPU in 16.237 seconds (90% CPU, 9153069 Lips)
+% 109,051,925 inferences, 12.945 CPU in 12.980 seconds (100% CPU, 8424321 Lips)
+% Started at Wed May 15 09:19:47 2019
+% 152.156 seconds cpu time for 1,298,228,497 inferences
+% 7,458 atoms, 4,443 functors, 3,443 predicates, 69 modules, 130,610 VM-codes
+% 
+%                     Limit   Allocated      In use
+% Local  stack:           -       24 Kb    1,752  b
+% Global stack:           -    6,351 Mb    2,626 Mb
+% Trail  stack:           -    1,841 Mb    3,856  b
+%        Total:    8,192 Mb    8,192 Mb    2,626 Mb
+% 
+% 19 garbage collections gained 18,003,118,520 bytes in 27.139 seconds.
+% 3 clause garbage collections gained 125 clauses in 0.000 seconds.
+% Stack shifts: 3 local, 19 global, 19 trail in 0.197 seconds
+% 2 threads, 0 finished threads used 0.000 seconds
+% 3 engines, 6 finished engines
+true.
 
 */
 
+test:lazy_nats_from_to(N,M,Ns) :- put_attr(Ns,test,state(N,M,_)).
+
+test:attr_unify_hook(State,Value) :-
+  State=state(N,M,Read),
+  N<M,
+  ( var(Read) ->
+      succ(N,SN),
+      test:lazy_nats_from_to(SN,M,Tail),
+      nb_setarg(3,State,[N|Tail]),
+      arg(3,State,Value)
+  ;
+      Value = Read
+  ).
+  
